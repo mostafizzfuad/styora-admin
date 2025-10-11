253c18f192a1a3ab9c169f4a88f5b422526af169
Mostafizur <mostafizzfuad@gmail.com>
2025-10-11 16:48:29 +0600

    feat(order): create order using Stripe webhook

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 	typescript: true,
 });
 
