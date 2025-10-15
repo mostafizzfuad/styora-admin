001d170cbe3ec9829570fa643dfba87ffe09fcef
Mostafizur <mostafizzfuad@gmail.com>
2025-10-15 12:48:23 +0600

    feat(product): add related products API

---
+++ app/api/products/[productId]/related/route.ts
@@ -0,0 +1,40 @@
+import Product from "@/lib/models/Product";
+import { connectToDB } from "@/lib/mongoDB";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+	_req: NextRequest,
+	{ params }: { params: Promise<{ productId: string }> }
+) => {
+	try {
+		await connectToDB();
+		const { productId } = await params;
+		const product = await Product.findById(productId);
+
+		if (!product) {
+			return new NextResponse(
+				JSON.stringify({ message: "Product not found" }),
+				{ status: 404 }
+			);
+		}
+
+		const relatedProducts = await Product.find({
+			$or: [
+				{ category: product.category },
+				{ collections: { $in: product.collections } },
+			],
+			_id: { $ne: product._id }, // Exclude the current product
+		});
+
+		if (!relatedProducts) {
+			return new NextResponse(
+				JSON.stringify({ message: "No related products found" }),
+				{ status: 404 }
+			);
+		}
+		return NextResponse.json(relatedProducts, { status: 200 });
+	} catch (err) {
+		console.log("[related_GET]", err);
+		return new NextResponse("Internal server error", { status: 500 });
+	}
+};
